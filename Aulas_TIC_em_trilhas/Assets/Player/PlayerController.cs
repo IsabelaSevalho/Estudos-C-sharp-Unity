195e5cca693b02e9ad39ecac4f31347d9800d261
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-05-02 11:38:07 -0300

    Adding motion settings and other input manager settings

---
+++ Aulas_TIC_em_trilhas/Assets/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    PlayerInputs inputActions;
+
+    // Start is called before the first frame update
+    //Awake é chamado antes do start
+    void Awake()
+    {
+        //Debug.Log("Awake está funcionando");
+        inputActions = new PlayerInputs();
+    }
+
+    private void OnEnable()
+    {
+        //inicializar
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        //desativar
+        inputActions.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update está funcionando");
+
+        //verifica o valor do botão que está clicando
+        var moveInputs = inputActions.Player_Map.Movement.ReadValue<Vector2>();
+
+        Debug.Log("Move inputs: " + moveInputs);
+    }
+}
