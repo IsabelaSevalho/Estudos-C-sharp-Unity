637f92d9116bb81bc900a3bbdcc0bf693a95c99d
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2025-05-28 10:39:55 -0300

    Trying to make a transition between animations

--- Testando/Assets/Scripts/MyScript.cs
+++ Testando/Assets/Scripts/MyScript.cs
@@ -12,8 +12,13 @@ public class MyScript : MonoBehaviour
     public Button sendButton;
     public TextMeshProUGUI responseText;
 
+    //para transicionar animações
+    public Animator animator;
+    public string[] animationNames;
+
     void Start()
     {
+        animator = GetComponent<Animator>();
         // Liga o botão à função de envio
         sendButton.onClick.AddListener(OnSendButtonClick);
 
@@ -35,10 +40,28 @@ public class MyScript : MonoBehaviour
         // Mostra a resposta no console
         responseText.text = "Bot: " + reply;
         Debug.Log("Resposta do modelo: " + reply);
+
+        if (reply.Contains("Hi") || reply.Contains("Hello"))
+            PlayAnimationByIndex(1); // wave
+
+        else if (reply.Contains("Bye") || reply.Contains("See you later"))
+            PlayAnimationByIndex(2); //walk
+
+        else
+            PlayAnimationByIndex(0);
     }
 
     void ReplyCompleted() //opcional, chamado quando a resposta está completa
     {
         Debug.Log("Resposta completa!");
+    }
+
+    //animação
+    public void PlayAnimationByIndex(int index)
+    {
+        if (index >= 0 && index < animationNames.Length)
+        {
+            animator.Play(animationNames[index]);
+        }
     }
 }
